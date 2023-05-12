82ca5eb3ca4f43ad21f2cbb25a99d1c02b1b2983
Bishnu <thapabishnu20@gmail.com>
2023-05-12 14:06:06 +0545

    simple movies listing web app

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Movies from './Movies'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const increment = () => {
-    setCount(count+1)
-  }
-  const decrement = () => {
-    if (count>0)
-      setCount(count-1)
-  }
+  // const [count, setCount] = useState(0)
+  // const increment = () => {
+  //   setCount(count+1)
+  // }
+  // const decrement = () => {
+  //   if (count>0)
+  //     setCount(count-1)
+  // }
   return (
     <>
-      <h1>Hello From React Javascript course</h1>
+      {/* <h1>Hello From React Javascript course</h1>
       <h4>Count:{count}</h4>
       <button onClick={increment} >Increment</button>
-      <button onClick={decrement} >Decrement</button>
+      <button onClick={decrement} >Decrement</button> */}
+      <Movies />
     </>
   )
 }
---
+++ src/Movies.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Movies = () => {
+  return (
+    <>
+          <h2>Thapa Technical Netflix Pick</h2>
+          <p>List of 5 Best Series</p>
+          <ol>
+              <li>Dark</li>
+              <li>Mr. Robot</li>
+              <li>The Social Network</li>
+              <li>Extra Curricular</li>
+              <li>Mirzapur</li>
+          </ol>
+    </>
+  )
+}
+
+export default Movies
